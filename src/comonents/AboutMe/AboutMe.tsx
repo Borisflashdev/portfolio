d38df00673fdd9f1fe2cc644ea4e277ab7249734
Boris <borismirkovic11@gmail.com>
2025-11-16 02:38:31 +0100

    chore: change Contact div

--- src/comonents/AboutMe/AboutMe.tsx
+++ src/comonents/AboutMe/AboutMe.tsx
@@ -11,10 +11,15 @@ const AboutMe = () => {
                     <div className="flex justify-center">
                         <img src="/character.gif" alt="Boris Coding" className="w-50" onContextMenu={(e) => e.preventDefault()} draggable={false} />
                     </div>
-                    <p>---------- Contact ----------</p>
+                    <div className="flex items-center text-center">
+                        <hr className="flex-1 border-t-2 border-dashed" />
+                        <span className="px-4">Contact</span>
+                        <hr className="flex-1 border-t-2 border-dashed " />
+                    </div>
                     <div className="flex flex-col items-start text-box">
                         <p>
-                            &lt;<span className="mc">Location</span>&gt; <a href="https://maps.app.goo.gl/4wJ5JahntsGnpxWC7" className="link">Belgrade, Serbia</a>
+                            &lt;<span className="mc">Location</span>&gt; <a
+                            href="https://maps.app.goo.gl/4wJ5JahntsGnpxWC7" className="link">Belgrade, Serbia</a>
                         </p>
                         <p>
                             &lt;<span className="mc">Email</span>&gt; <a href="https://mail.google.com/mail/?view=cm&fs=1&to=borismirkovic11@gmail.com" className="link">borismirkovic11@gmail.com</a>
