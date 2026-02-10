50f9ec79952b25d729b5316006b51152344557aa
Boris <borismirkovic11@gmail.com>
2026-02-10 01:10:54 +0100

    fix: font size on mobile

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -14,22 +14,35 @@ const AboutMe = () => {
                         <span className="px-4">Contact</span>
                         <hr className="flex-1 border-t-2 border-dashed " />
                     </div>
-                    <div className="flex flex-col items-start text-base max-[400px]:text-xs">
-                        <p>
-                            &lt;<span className="text-[#00E5FF]">Location</span>&gt; <a
-                            href="https://maps.app.goo.gl/4wJ5JahntsGnpxWC7" className="hover:text-[#00E5FF] hover:underline">Belgrade, Serbia</a>
+                    <div className="flex flex-col items-start text-base max-[400px]:text-xs w-full overflow-hidden">
+                        <p className="flex w-full max-w-full whitespace-nowrap overflow-hidden">
+                            <span className="text-[#00E5FF] shrink-0">Email</span>
+                        <a
+                            href="https://mail.google.com/mail/?view=cm&fs=1&to=borismirkovic11@gmail.com"
+                            className="hover:text-[#00E5FF] hover:underline ml-1 truncate"
+                        >
+                            borismirkovic11@gmail.com
+                        </a>
+                    </p>
+                    <p className="flex w-full max-w-full whitespace-nowrap overflow-hidden">
+                        <span className="text-[#00E5FF] shrink-0">Linkedin</span>
+                        <a href="https://www.linkedin.com/in/boris-mirkovic/" className="hover:text-[#00E5FF] hover:underline ml-1 truncate">
+                            in/boris-mirkovic/
+                        </a>
+                    </p>
+                        <p className="flex w-full max-w-full whitespace-nowrap overflow-hidden">
+                            <span className="text-[#00E5FF] shrink-0">Github</span>
+                            <a href="https://github.com/Borisflashdev" className="hover:text-[#00E5FF] hover:underline ml-1 truncate">
+                                Borisflashdev
+                            </a>
                         </p>
-                        <p>
-                            &lt;<span className="text-[#00E5FF]">Email</span>&gt; <a href="https://mail.google.com/mail/?view=cm&fs=1&to=borismirkovic11@gmail.com" className="hover:text-[#00E5FF] hover:underline">borismirkovic11@gmail.com</a>
+                        <p className="flex w-full max-w-full">
+                            <span className="text-[#00E5FF] shrink-0">Location</span>
+                            <span className="ml-1 truncate">Belgrade, Serbia</span>
                         </p>
-                        <p>
-                            &lt;<span className="text-[#00E5FF]">Linkedin</span>&gt; <a href="https://www.linkedin.com/in/boris-mirkovic/" className="hover:text-[#00E5FF] hover:underline">in/boris-mirkovic/</a>
-                        </p>
-                        <p>
-                            &lt;<span className="text-[#00E5FF]">Github</span>&gt; <a href="https://github.com/Borisflashdev" className="hover:text-[#00E5FF] hover:underline">Borisflashdev</a>
-                        </p>
-                        <p>
-                            &lt;<span className="text-[#00E5FF]">Resume</span>&gt; <a href="/resume.pdf" className="hover:text-[#00E5FF] hover:underline">download</a>
+                        <p className="flex w-full max-w-full">
+                            <span className="text-[#00E5FF] shrink-0">Resume</span>
+                            <a href="/resume.pdf" className="hover:text-[#00E5FF] hover:underline ml-1">download</a>
                         </p>
                     </div>
                 </div>
